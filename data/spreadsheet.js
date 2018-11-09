de6582f71e53437c77c6b44a342265e82fff39ea
ethanryan <ethanryan@gmail.com>
2018-11-09 12:51:58 -0500

    updated spreadsheet file with spreadsheet id

--- data/spreadsheet.js
+++ data/spreadsheet.js
@@ -2,7 +2,7 @@ var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('./client_secret.json');
 
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
-var doc = new GoogleSpreadsheet('*spreadsheet ID*');
+var doc = new GoogleSpreadsheet('1tpzXkd0eMJZtAMo8lZ2gvcqCMx_ku4kx_FUOZvh8x20');
 
 // Authenticate with the Google Spreadsheets API.
 doc.useServiceAccountAuth(creds, function (err) {
