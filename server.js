ef10eb1cd108e0d5e8551bc5d8ac2bd60e423d4e
ethanryan <ethanryan@gmail.com>
2018-11-11 12:08:21 -0500

    updated get request with conditional based on req.query.order passed from frontend

--- server.js
+++ server.js
@@ -75,13 +75,21 @@ app.get("/clicked", (req, res) => {
 });
 
 app.get("/presidents", (req, res) => {
+
+  console.log("req.query.order is: ", req.query.order)
+
   // Authenticate with the Google Spreadsheets API.
   doc.useServiceAccountAuth(creds, function (err) {
     // Get all of the rows from the spreadsheet.
       doc.getRows(1, function (err, rows) {
         if(!err) {
           // var sortedArray = sortArrayAscendingOrder(rows)
-          var sortedArray = sortArrayAscendingOrder(rows)
+          var sortedArray
+          if (req.query.order === "orderAscending") {
+            sortedArray = sortArrayAscendingOrder(rows)
+          } else {
+            sortedArray = sortArrayDescendingOrder(rows)
+          }
           res.json(sortedArray) //send rows of data as response to api endpoint
         } else {
           console.log("err is: ", err)
