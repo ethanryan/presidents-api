ad3a948fab57667138545c61c3d42f63e57a15dd
ethanryan <ethanryan@gmail.com>
2018-11-27 01:43:13 -0500

    cleaning up code

--- server.js
+++ server.js
@@ -17,54 +17,26 @@ app.listen(port, () => {
   console.log("Server running on port:", port);
 });
 
-// function compareNamesAscending(a,b) {
-//   if (a.president < b.president)
-//     return -1;
-//   if (a.president > b.president)
-//     return 1;
-//   return 0;
-// }
-//
-// function compareNamesDescending(a,b) {
-//   if (a.president < b.president)
-//     return 1;
-//   if (a.president > b.president)
-//     return -1;
-//   return 0;
-// }
-
 function sortByKey(array, key) {
-    return array.sort(function(a, b) {
-        var x = a[key];
-        var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+  return array.sort(function(a, b) {
+    var x = a[key];
+    var y = b[key];
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
 }
 
 function sortArray(array, order) {
-  console.log('in sortArray, order is: ', order)
   if (order === "orderAscending") {
-    // return array.sort(compareNamesAscending)
     return sortByKey(array, "president")
-    // return array.sort(compareNames(order) ) //check how to refactor this with just one function
   } else {
-    // return array.sort(compareNamesDescending)
     return sortByKey(array, "president").reverse()
   }
 }
 
 app.get("/presidents/:order", (req, res) => {
-
   // console.log("req.params.order is: ", req.params.order) //this is with :order
   // console.log("req.query.order is: ", req.query.order) //this is with ?order
   var order = req.params.order
-  console.log("1. order is: ", order) //this is with :order
-
   var sortedArray = sortArray(prezData, order)
-  console.log('1. sortedArray is: ', sortedArray)
-  res.json(sortedArray) //send rows of data as response to api endpoint
-
+  res.json(sortedArray) //send sortedArray as response to api endpoint
 })
-
-// var sortedArrayTest = sortArray(prezData, "orderAscending")
-// console.log('1. sortedArrayTest is: ', sortedArrayTest)
